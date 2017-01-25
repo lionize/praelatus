effc786d712781aa7e2a72ea716ff35730ed9b13
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-01-25 11:28:25 -0500

    renamed

---
+++ backend/migrations/migration_1.sql
@@ -0,0 +1,27 @@
+/* 1 */
+create table if not exists db_info(
+    db_version integer,
+);
+
+insert into db_info (db_version) VALUES (1);
+
+create table if not exists users (
+    id         serial primary key,
+    email      varchar(100) NOT NULL UNIQUE,
+    username   varchar(100) NOT NULL UNIQUE,
+    password   varchar(250) NOT NULL,
+    fullName   varchar(250) NOT NULL,
+    profilePic varchar(250) NOT NULL
+);
+
+create table if not exists jobs (
+    id    serial primary key,
+    name  varchar(250) NOT NULL,
+    body  varchar(250) NOT NULL,
+    owner integer references users(id)
+);
+
+create table if not exists job_notifications (
+    job_id  integer references jobs(id) NOT NULL,
+    user_id integer references users(id) NOT NULL
+);
