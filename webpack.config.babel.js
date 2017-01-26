3362b3619a9948181d2c46e421acbc7c264725d7
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-26 20:22:20 +0000

    Create webpack config file

---
+++ webpack.config.babel.js
@@ -0,0 +1,47 @@
+import path from 'path'
+import webpack from 'webpack'
+
+export default {
+  entry: [
+    'babel-polyfill',
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    './src/index.js'
+  ],
+
+  output: {
+    filename: 'index.js',
+    publicPath: '/static/',
+    path: process.env.NODE_ENV === 'production'
+      ? path.resolve(__dirname, 'build', 'release', 'static')
+      : path.resolve(__dirname, 'build', 'debug', 'static')
+  },
+
+  devtool: 'inline-source-map',
+  debug: true,
+
+  devServer: {
+    historyApiFallback: {
+      index: 'index.html'
+    }
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+
+  resolve: {
+    root: [path.resolve(__dirname, 'frontend'), path.resolve(__dirname, 'node_modules')],
+    extensions: ['', '.js']
+  }
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
