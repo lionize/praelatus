694352dd841e1cea3830851eae07ae43dfbfe2af
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-26 19:32:25 -0500

    Rename CreateStore.js to createStore.js

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import configureStore from 'app/redux/CreateStore'
+import configureStore from 'app/redux/createStore'
 
 export default () => {
   const rootReducer = combineReducers({})
