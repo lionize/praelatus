231e76673608b8d2010e924f4fc1b4c801be4479
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-26 20:41:43 -0500

    Fix jsx-filename-extension rule name in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,7 +48,7 @@ module.exports = {
     'import/imports-first': 0,
     'import/prefer-default-export': 0,
 
-    'react/jsx-filename-extensions': 0,
+    'react/jsx-filename-extension': 0,
     'react/forbid-prop-types': 0,
     'react/prop-types': 0,
 
