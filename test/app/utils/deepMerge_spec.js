678b750201a8c39c5023beaad12fd036b1ce3dfe
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-27 14:06:15 -0500

    Rename app/util -> app/utils

--- test/app/utils/deepMerge_spec.js
+++ test/app/utils/deepMerge_spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import deepMerge from 'app/util/deepMerge'
+import deepMerge from 'app/utils/deepMerge'
 
 describe('deepMerge', () => {
   describe('with two arrays', () => {
