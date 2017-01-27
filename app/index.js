0a5ed33f20e8dc2719a0f67b5c7666a9b59b6291
Mark Chandler <mark.allen.chandler@gmail.com>
2017-01-26 20:37:20 -0500

    Move App and Root to app/containers

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import createStore from 'app/redux'
-import Root from 'app/Root'
+import Root from 'app/containers/Root'
 
 const store = createStore()
 
